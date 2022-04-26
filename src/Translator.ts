2a526c91241200dc4905d6aca8c763cc30c1dfb5
Xabi Etxeberria <xabier.etxeberria93@gmail.com>
2022-04-26 12:25:57 +0200

    Add instruction to function mapping

--- src/Translator.ts
+++ src/Translator.ts
@@ -3,6 +3,16 @@ import * as fs from "fs";
 type Instruction = "👉" | "👈" | "👆" | "👇" | "🤜" | "🤛" | "👊";
 
 export class Translator {
+  private readonly instructions = new Map<Instruction, Function>([
+    ["👉", () => {}],
+    ["👈", () => {}],
+    ["👆", () => {}],
+    ["👇", () => {}],
+    ["🤜", () => {}],
+    ["🤛", () => {}],
+    ["👊", () => {}]
+  ]);
+
   async run(inputFilePath: string): Promise<string> {
     const fileContent = (await fs.promises.readFile(inputFilePath)).toString().trim();
     const fileInstructions = Array.from(fileContent) as Array<Instruction>;
