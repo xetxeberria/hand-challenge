499c90e2c967d5f3f63ac238c136e9b16db1694d
Xabi Etxeberria <xabier.etxeberria93@gmail.com>
2022-04-26 12:21:50 +0200

    Read file content as an array of instructions

--- src/Translator.ts
+++ src/Translator.ts
@@ -1,5 +1,14 @@
+import * as fs from "fs";
+
+type Instruction = "👉" | "👈" | "👆" | "👇" | "🤜" | "🤛" | "👊";
+
 export class Translator {
   async run(inputFilePath: string): Promise<string> {
+    const fileContent = (await fs.promises.readFile(inputFilePath)).toString().trim();
+    const fileInstructions = Array.from(fileContent) as Array<Instruction>;
+
+    console.log(fileInstructions);
+
     return "";
   }
 }
